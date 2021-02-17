d278995f632c2c335097af109f0d0b28bccb4d42
SergeyTokareff <sergey.v.tokareff@gmail.com>
2021-02-17 11:11:28 +0200

    fix: fix bugs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
   return Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
   return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
   return array.reduce((a, b) => (a + b)) / array.length;
 }
